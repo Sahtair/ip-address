ceddc732c297ab3e7a905af15bc825b581a4433d
jurest <jure.stepisnik@smartis.si>
2018-11-06 12:55:48 +0100

    Finished inserting into mongo

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const mongodb = require('mongodb').MongoClient;
+const request = require('request-promise-native');
 
 const port = 3000;
 const dbName = 'blubblubTest';
 let db = {};
+
+const ipAddressApiUrl = 'https://api.ipify.org?format=json';
 
 const client = new mongodb('mongodb://localhost:27017');
 
@@ -20,6 +23,33 @@ app.get('/', async (req, res) => {
     }
     res.send(response)
 });
+
+app.get('/status', async (req, res) => {
+    const addressesCollection = db.collection('addresses');
+
+    const ipApiResponse = await request(ipAddressApiUrl)
+        .catch(err => console.log(`Error making ip request: ${err}`));
+
+    const parsedIpApiResponse = JSON.parse(ipApiResponse);
+    const ipAddress = parsedIpApiResponse.ip
+
+    console.log(`Ip address: ${ipAddress}`);
+    const dbResponse = await addressesCollection.findOne({
+        ipAddress
+    });
+
+    console.log(`Found address: ${JSON.stringify(dbResponse)}`);
+
+    if (dbResponse !== null) {
+        console.log(`Found record: ${dbResponse}`)
+        res.send(dbResponse);
+    } else {
+        await addressesCollection.insertOne({
+            ipAddress
+        }).catch(err => console.log(`Error inserting record: ${err}`));
+        res.send(ipAddress);
+    }
+})
 
 client.connect((err, database) => {
     if (err) {
